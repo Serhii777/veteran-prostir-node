a81bce5286fdd9277f0435fe47d775b5bae5eded
Serhii <muzykasv72@gmail.com>
2022-01-09 01:58:40 +0200

    Change package3

--- api/server.js
+++ api/server.js
@@ -28,13 +28,14 @@ const initDatabase = require("./helpers/initDatabase");
 const app = express();
 global.__basedir = __dirname;
 
-console.log('process.env.SITE_DOMAIN_LOCAL:', process.env.SITE_DOMAIN_LOCAL);
+// console.log("process.env.SITE_DOMAIN_LOCAL:", process.env.SITE_DOMAIN_LOCAL);
+// console.log("process.env.SITE_DOMAIN:", `${process.env.SITE_DOMAIN}`);
 
 const corsOptions = {
   // origin: "http://localhost:3000",
   // origin: "https://veteran-prostir-vn.netlify.app",
-  origin: process.env.SITE_DOMAIN_LOCAL,
-  origin: process.env.SITE_DOMAIN,
+  origin: `${process.env.SITE_DOMAIN_LOCAL}`,
+  origin: `${process.env.SITE_DOMAIN}`,
 };
 
 // const host = process.env.HOST;
